547112ff191726f77f9d8657ed50dc085f5edc27
daxmagex <daxnoob@hotmail.com>
2016-11-05 15:29:48 -0400

    A

--- src/Driver.java
+++ src/Driver.java
@@ -14,7 +14,7 @@ public class Driver extends Application{
     public void start(Stage primaryStage) throws Exception {
         //FCSJRRFIFSJDJNISJ
         Parent root = FXMLLoader.load(getClass().getResource("Main Screen.fxml"));
-
+            //seocnonco
         Scene scene = new Scene(root);
 
         primaryStage.setTitle("Suggest Me Food!");
