29834fdb9fb847f9ecaf70d323348dca592adc2e
daxmagex <daxnoob@hotmail.com>
2016-11-05 14:42:27 -0400

    a
    weilin

---
+++ src/Driver.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Weilin on 11/5/2016.
+ */
+public class Driver extends Application{
+
+    public static void main(String[] args){
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
