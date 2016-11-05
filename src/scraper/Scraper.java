10044df737096ae41697678b9e4897d68b731ad1
kennyzha <kzhao7301@gmail.com>
2016-11-05 15:07:01 -0400

    added chromedriver

--- src/scraper/Scraper.java
+++ src/scraper/Scraper.java
@@ -1,8 +1,21 @@
 package scraper;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Created by Kenny on 11/5/2016.
  */
 public class Scraper
 {
+    public static void main(String[] args) {
+        Scraper s = new Scraper();
+        s.scrape();
+    }
+    public void scrape(){
+        System.out.println(this.getClass().getResource("chromedriver.exe").getFile());
+        System.setProperty("webdriver.chrome.driver", this.getClass().getResource("chromedriver.exe").getFile());
+        WebDriver webDrive = new ChromeDriver();
+    }
 }
+
