9a0c2cb1cb5da78348c1d3a9a75de1b4a6c2ee0f
daxmagex <daxnoob@hotmail.com>
2016-11-05 14:46:49 -0400

    aa
    driver update

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,9 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- * Created by Weilin on 11/5/2016.
- */
 public class Driver extends Application{
 
     public static void main(String[] args){
@@ -13,5 +13,12 @@ public class Driver extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        Parent root = FXMLLoader.load(getClass().getResource("Main Screen.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Suggest Me Food!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
