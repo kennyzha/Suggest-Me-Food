38387573aa224d0fcb9cc0f4deb29dbd8a65433b
daxmagex <daxnoob@hotmail.com>
2016-11-05 15:28:40 -0400

    A

--- src/Driver.java
+++ src/Driver.java
@@ -12,7 +12,7 @@ public class Driver extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        //FCSJRRFIFSJDJNISJ
         Parent root = FXMLLoader.load(getClass().getResource("Main Screen.fxml"));
 
         Scene scene = new Scene(root);
